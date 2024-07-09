191f0319dd405019941481150576ec57a8d43a3d
Abhay Kumar Sethi <sethi.abhay.7876@gmail.com>
2024-07-09 17:38:21 +0000

    functions part2 done

---
+++ 03_Functions/02_Functions_Objects.js
@@ -0,0 +1,31 @@
+function calculateCartPrice(...num1){
+    //... is called rest and spred operator name depends on usage.
+return num1;
+}
+
+ console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+//node 02_Functions_Objects.js
---
+++ 03_Functions/03_This_Arrow_Function.js
@@ -0,0 +1,10 @@
+//node 03_This_Arrow_Function.js
+function one(){
+    const username = "abhay"
+    function two(){
+        const webstite="youtube";
+        console.log(username)
+    }
+    two()
+}
+one()
